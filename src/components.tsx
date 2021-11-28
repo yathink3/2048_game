f4c54a46128d9acdb2ba15f676179d63bb5061a8
yathin <yathink3@gmail.com>
2021-11-28 20:53:58 +0530

    theme applied

--- src/components.tsx
+++ src/components.tsx
@@ -31,7 +31,7 @@ export const GameOverPrompt = ({ title, description, actions }: { title?: ReactN
 );
 
 export const Box = ({ value }: { value: number | null }) => {
-  const style = { 2: 'bg-gray-500', 4: 'bg-blue-300', 8: 'bg-green-300', 16: 'bg-blue-500', 32: 'bg-purple-900', 64: 'bg-red-300', 128: 'bg-yellow-200', 256: 'bg-yellow-300', 512: 'bg-indigo-300', 1024: 'bg-pink-300', 2048: 'bg-gray-300' }[value || 0] || 'bg-gray-100';
+  const style = { 2: 'bg-gray-500', 4: 'bg-blue-300', 8: 'bg-green-300', 16: 'bg-blue-500', 32: 'bg-purple-900', 64: 'bg-red-300', 128: 'bg-yellow-200', 256: 'bg-yellow-300', 512: 'bg-indigo-300', 1024: 'bg-pink-300', 2048: 'bg-gray-300' }[value || 0] || 'bg-gray-100 dark:bg-gray-800';
   return <div className={`flex rounded-lg  justify-center items-center w-20 h-20 lg:w-24 lg:h-24 m-0.5 ${style} text-white sm:text-2xl lg:text-6xl font-semibold border-solid border border-gray-300`}>{value}</div>;
 };
 
@@ -46,27 +46,27 @@ export const Board = ({ board }: { board: Array<number | null> }) => (
 export const ArrowKeySet = ({ handleKey }: { handleKey: (v: string) => any }) => (
   <div className='grid grid-cols-3 md:hidden'>
     <span></span>
-    <button className='rounded-lg border-4 border-gray-100' onClick={e => handleKey('up')}>
+    <button className='rounded-lg border-4 border-gray-100 dark:text-white' onClick={e => handleKey('up')}>
       <svg width='55' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64'>
-        <path fill='none' stroke='#202020' strokeMiterlimit='10' strokeWidth='2' d='M20 40l11.994-14L44 40'></path>
+        <path className='fill-current' stroke='#202020' strokeMiterlimit='10' strokeWidth='2' d='M20 40l11.994-14L44 40'></path>
       </svg>
     </button>
     <span></span>
-    <button className='rounded-lg border-4 border-gray-100' onClick={e => handleKey('left')}>
+    <button className='rounded-lg border-4 border-gray-100 dark:text-white' onClick={e => handleKey('left')}>
       <svg width='55' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64'>
-        <path fill='none' stroke='#202020' strokeMiterlimit='10' strokeWidth='2' d='M39 20.006L25 32l14 12.006'></path>
+        <path className='fill-current' stroke='#202020' strokeMiterlimit='10' strokeWidth='2' d='M39 20.006L25 32l14 12.006'></path>
       </svg>
     </button>
     <span></span>
-    <button className='rounded-lg border-4 border-gray-100' onClick={e => handleKey('right')}>
+    <button className='rounded-lg border-4 border-gray-100 dark:text-white' onClick={e => handleKey('right')}>
       <svg width='55' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64'>
-        <path fill='none' stroke='#202020' strokeMiterlimit='10' strokeWidth='2' d='M26 20.006L40 32 26 44.006'></path>
+        <path className='fill-current' stroke='#202020' strokeMiterlimit='10' strokeWidth='2' d='M26 20.006L40 32 26 44.006'></path>
       </svg>
     </button>
     <span></span>
-    <button className='rounded-lg border-4 border-gray-100' onClick={e => handleKey('down')}>
+    <button className='rounded-lg border-4 border-gray-100 dark:text-white' onClick={e => handleKey('down')}>
       <svg width='55' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64'>
-        <path fill='none' stroke='#202020' strokeMiterlimit='10' strokeWidth='2' d='M20 26l11.994 14L44 26'></path>
+        <path className='fill-current' stroke='#202020' strokeMiterlimit='10' strokeWidth='2' d='M20 26l11.994 14L44 26'></path>
       </svg>
     </button>
     <span></span>
