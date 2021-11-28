799d7742af7ba8f8c77ed21f3c8af613c742525e
yathin <yathink3@gmail.com>
2021-11-28 13:18:56 +0530

    utils chnaged

--- src/utils.ts
+++ src/utils.ts
@@ -33,8 +33,7 @@ export const renderBoard = (board: Array<number | null>, move: string) => {
       if (['left', 'right'].includes(move)) row.push(board[i * 4 + j]);
       if (['up', 'down'].includes(move)) row.push(board[j * 4 + i]);
     }
-    scoreVal = scoreVal + addRow(row, move);
-    moveRow(row, move);
+    scoreVal = scoreVal + addAndSortRow(row, move);
     for (let j = 0; j < 4; j++) {
       if (['left', 'right'].includes(move)) newBoard[i * 4 + j] = row[j];
       if (['up', 'down'].includes(move)) newBoard[j * 4 + i] = row[j];
@@ -43,38 +42,25 @@ export const renderBoard = (board: Array<number | null>, move: string) => {
   return { newBoard, scoreVal };
 };
 
-const addRow = (row: Array<number | null>, move: string) => {
+const addAndSortRow = (row: Array<number | null>, move: string) => {
   let score = 0;
   let current = 0;
   let currentIdx = 0;
-  for (let i = 0; i < row.length; i++) {
-    let j = ['right', 'down'].includes(move) ? row.length - i - 1 : i;
-    const ele = row[j];
-    if (current === ele) {
-      row[j] = null;
+  if (['right', 'down'].includes(move)) row.reverse();
+  row.forEach((v, i) => {
+    if (current === v) {
+      row[i] = null;
       row[currentIdx] = current * 2;
       score = score + current * 2;
       current = 0;
       currentIdx = 0;
-    } else if (ele !== null) {
-      current = ele;
-      currentIdx = j;
+    } else if (v !== null) {
+      current = v;
+      currentIdx = i;
     }
-  }
-  return score;
-};
+  });
+  row.sort((a, b) => (b === null ? -1 : 0));
+  if (['right', 'down'].includes(move)) row.reverse();
 
-const moveRow = (row: Array<number | null>, move: string) => {
-  if (['left', 'up'].includes(move))
-    row.sort((a, b) => {
-      if ((b || 0) > (a || 0)) return 0;
-      if (b == null) return -1;
-      else return 0;
-    });
-  if (['right', 'down'].includes(move))
-    row.sort((a, b) => {
-      if ((a || 0) > (b || 0)) return 0;
-      if (a == null) return -1;
-      else return 0;
-    });
+  return score;
 };
