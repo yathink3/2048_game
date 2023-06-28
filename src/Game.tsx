b99884f52f51f5d52a3895b12b4a5cbd897680cc
Yathin k <52459137+yathink3@users.noreply.github.com>
2023-06-28 12:33:52 +0530

    Update Game.tsx

--- src/Game.tsx
+++ src/Game.tsx
@@ -11,14 +11,16 @@ const Game = ({ CROSS_NUMBER = 4, WINNING_NUMBER = 2048 }: { CROSS_NUMBER?: numb
   const resetGame = () => void (initializeBoard(), setMove(''));
 
   return (
-    <div className='fixed md:relative flex flex-col min-h-screen min-w-full justify-evenly items-center border-0 focus:outline-none noselect dark:bg-gray-800'>
+    <div className='flex flex-col min-h-screen min-w-full justify-evenly items-center border-0 focus:outline-none noselect dark:bg-gray-800'>
       {status === 'lost' && <GameOverPrompt title='Game Over' description={`Game ended, You Lost the Match. Your Score : ${score}`} actions={<Button name='New Game' handleClick={resetGame} />} />}
       {status === 'won' && <GameOverPrompt title='You won!' description='Game ended, You won the Match.' actions={<Button name='New Game' handleClick={resetGame} />} />}
-      <div className='flex flex-row justify-evenly dark:text-white'>
-        <div className='hidden md:block'>Use arrow keys to merge tiles and reach the 2048 tile.</div>
-        <div className='hidden'>Use ↓ ← ↑ → arrows to merge tiles and reach 2048.</div>
+      <div className='flex flex-row justify-evenly hidden md:block'>
+        <div className='dark:text-white'>Use arrow keys to merge tiles and reach the 2048 tile.</div>
       </div>
-      <div className='flex flex-row justify-evenly '>
+      <div className='flex flex-row justify-evenly hidden'>
+        <div className='dark:text-white'>Use ↓ ← ↑ → arrows to merge tiles and reach 2048.</div>
+      </div>
+      <div className='flex flex-row justify-evenly'>
         <span className='py-2 px-4 rounded-lg border-4 border-gray-200 items-center m-1 dark:text-white'>SCORE : {score}</span>
         <span className='py-2 px-4 rounded-lg border-4 border-gray-200 items-center m-1 dark:text-white'>BEST : {best}</span>
       </div>
